90223b7fab339337c1b627510036300a914110f3
sgajbhiye96 <sgajbhiye96@gmail.com>
2025-10-04 08:21:00 +0530

    Version -1

---
+++ new.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+
+# print(factorial(5))
+
+# git --version
+# git init 
+# git config --global user.name "Your Name"
+# git config --global user.email "youremail@example.com"
